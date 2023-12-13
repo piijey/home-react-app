2ead41cc6eaf4a5e55602f69b5c20d098b1f26c1
piijey <pj.223f@gmail.com>
2023-12-14 01:01:17 +0900

    more fun game

--- src/App.js
+++ src/App.js
@@ -11,11 +11,13 @@ const Title = () => (
   );
 
 const Footer = () => (
-  <><p className="footer">
+  <>
+    <div className="footer">
     <p className="cc">
       Sounds: <a href="https://freesound.org/people/DrinkingWindGames/sounds/572954/">"Chicks" by DrinkingWindGames, Freesound</a> (<a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>), <a href="https://freesound.org/people/mickleness/sounds/269198/">"game-win.mp3" by mickleness, Freesound</a> (<a href="https://creativecommons.org/publicdomain/zero/1.0/">CC0 1.0</a>).
     </p>
-    <a href="https://github.com/piijey/home-react-app">このページのソースコードを見る (GitHub)</a></p>
+      <a href="https://github.com/piijey/home-react-app">このページのソースコードを見る (GitHub)</a>
+    </div>
   </>
   );
   
@@ -71,8 +73,9 @@ function SoundButton() {
   );
 }
 
+
 function Square({value, onSquareClick}) {
-  const colors = ['transparent', '#FBBE30', '#C72C65', '#55C2B7']
+  const colors = ['transparent', '#FBBE30', '#3CA69B', '#C72C65']
   const bgColor = colors[value] || 'transparent';
   return (
   <button
@@ -84,18 +87,23 @@ function Square({value, onSquareClick}) {
   );
 }
 
+let clickTime = 0;
 function Board(){
   const [squares, setSquares] = useState(initSquares());
+  const prevSquareRef = useRef(squares); //前回の squares
+  const [playAudio, setPlayAudio] = useState(false);
+  const timeoutRef = useRef(null); //setTimeout の id
 
   function initSquares() {
-    // 0~3 の値をランダムに、9個生成
+    // 0~3 の値（4未満の整数）をランダムに、9個生成
     return Array.from({ length: 9 }, () => Math.floor(Math.random() * 4))
   }
 
   function handleClick(i){
+    clickTime += 1;
     const nextSquares = squares.slice();
     if (nextSquares[i] === 3){
-      nextSquares[i] = 0;
+      nextSquares[i] = 1;
     } else{
       nextSquares[i] = nextSquares[i]+1;
     }
@@ -103,28 +111,91 @@ function Board(){
   }
 
   useEffect(() => {
+    const changedIndex = hasBoardChanged()
+    if (changedIndex != -1){
+      clearAligned(changedIndex);
+    }
+    prevSquareRef.current = squares;
+
     const allZeros = squares.every(square => square === 0);
-    if (allZeros) {
+    if (allZeros && !playAudio) {
+      setPlayAudio(true);
+    };
+  }, [squares]);
+
+  useEffect(() => {
+    if (playAudio) {
       let audio = new Audio('./sound/269198__mickleness__game-win.mp3');
       audio.play();
       audio.onended = () => {
         setSquares(initSquares())
+        clickTime = 0;
+        setPlayAudio(false);
       }
-    return () => {
-      audio.onended = null;
-    }
+      return () => {
+        audio.onended = null;
+      }
     };
-  }, [squares]);
+  }, [playAudio]);
+
+  function hasBoardChanged(){
+    for (let i = 0; i < squares.length; i++) {
+      if (squares[i] !== prevSquareRef.current[i]) {
+        return i //変化した（クリックされた）インデックスを返す
+      }
+    }
+    return -1; //変化がない場合
+  }
+
+  function clearAligned(changedIndex) {
+    let detected = false;
+    //縦横斜めのいずれかが揃ったら、値を 0 にする
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    const linesToCheck = lines.filter(line => line.includes(changedIndex))
+    const nextSquares = squares.slice();
+    for (let i = 0; i<linesToCheck.length; i++) {
+      const [a, b, c] = linesToCheck[i];
+      if (squares[a] != 0 && squares[a] === squares[b] && squares[a] === squares[c] ) {
+        detected = true;
+        nextSquares[a] = 0;
+        nextSquares[b] = 0;
+        nextSquares[c] = 0;
+      }
+    }
+    if (detected){
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+      timeoutRef.current = setTimeout(() => {
+        setSquares(nextSquares)
+        timeoutRef.current = null;
+      }, 400);
+    }
+  };
 
   return (
+  <>
     <div className="board-container">
       {[...Array(9)].map((_, index) => (
         <Square key={index} value={squares[index]} onSquareClick={() => handleClick(index)} />
       ))}
     </div>
+    <p className="caption">
+      {playAudio ? "おめでとう！" : "カラータイルをクリック！"}<br/>
+      クリックした回数: {clickTime}
+    </p>
+  </>
   )
 }
-
 
 const App = () => (
   <div className="app">
@@ -141,9 +212,6 @@ const App = () => (
       <div className="game">
         <Board />
       </div>
-      <p className="caption">
-      カラータイルをクリックして透明にしてね
-      </p>
     </div>
     <div className="card text-bg-light" style={{width: '18rem'}}>
       <div className="card-body">
